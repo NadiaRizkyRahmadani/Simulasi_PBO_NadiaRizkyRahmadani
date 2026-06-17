144293680ce42d3a04d91dfed8463fc390b337c5
HP 14S <nadiarizkyrahmadani@gmail.com>
2026-06-17 09:56:29 +0700

    [Tahap 1] Membuat Database dan Tabel

---
+++ db_simulasi_pbo_ti1c_nadiarizkyrahmadani.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 17, 2026 at 02:46 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_simulasi_pbo_ti1c_nadiarizkyrahmadani`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_pendaftaran`
+--
+
+CREATE TABLE `tabel_pendaftaran` (
+  `id_pendaftaran` int NOT NULL,
+  `nama_calon` varchar(100) NOT NULL,
+  `asal_sekolah` varchar(100) NOT NULL,
+  `nilai_ujian` decimal(5,2) NOT NULL,
+  `biaya_pendaftaran_dasar` decimal(10,2) NOT NULL,
+  `jalur_pendaftaran` enum('Reguler','Prestasi','Kedinasan') NOT NULL,
+  `pilihan_prodi` varchar(50) DEFAULT NULL,
+  `lokasi_kampus` varchar(50) DEFAULT NULL,
+  `jenis_prestasi` varchar(50) DEFAULT NULL,
+  `tingkat_prestasi` varchar(50) DEFAULT NULL,
+  `sk_ikatan_dinas` varchar(50) DEFAULT NULL,
+  `instansi_sponsor` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabel_pendaftaran`
+--
+ALTER TABLE `tabel_pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tabel_pendaftaran`
+--
+ALTER TABLE `tabel_pendaftaran`
+  MODIFY `id_pendaftaran` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
